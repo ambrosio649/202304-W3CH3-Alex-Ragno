bd21c14ba4138bce5acadd80ddc9adbe0dbca8e6
AlexRagno <alexragno4@gmail.com>
2023-05-17 21:54:30 +0200

    Add layout

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-new */
+import { Footer } from './components/footer';
+import { Header } from './components/header';
+import { Main } from './components/main';
+
+new Header('#app', 'Mascotas');
+new Main('#app');
+new Footer('#app', 'ambrosio ©️');
